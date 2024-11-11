2ebf4d249c3edfa994b4b6671cc332dc71686992
codedotking <huala.fun@outlook.com>
2024-11-11 23:59:16 +0800

    add users list

---
+++ app/api/user/list/route.ts
@@ -0,0 +1,12 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  const users = await db.user.findMany();
+
+  return NextResponse.json({
+    code: 200,
+    msg: "api",
+    data: users,
+  });
+};
